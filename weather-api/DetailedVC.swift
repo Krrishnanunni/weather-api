9edebb43fef0edd399386046f94018f0d9aabf13
Krrishnanunni <krishnanunni2004ka@gmail.com>
2025-07-22 22:44:35 +0530

    Everything Works

---
+++ weather-api/DetailedVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailedVC.swift
+//  weather-api
+//
+//  Created by Krishnanunni K A on 22/07/2025.
+//
+
+import UIKit
+
+class DetailedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
